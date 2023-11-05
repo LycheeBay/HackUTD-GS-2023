e67ee8f868020218060deea17c75dbc39ab2df75
SandyCreek <xiezhuoli771@gmail.com>
2023-11-05 03:52:41 -0600

    sentiment analysis pipe for reddit title

--- backend/reddit_parse.py
+++ backend/reddit_parse.py
@@ -1,4 +1,11 @@
 import ast
+from transformers import pipeline
+sentiment_pipeline = pipeline("sentiment-analysis")
+
+def sentiment(news_text):
+    parsed_sentiment = sentiment_pipeline(news_text.replace('\n', ' ').lower())
+    return parsed_sentiment
+
 with open("ibm-reddit-fetches.txt", "r", encoding = "utf-8") as f:
     for line in f:
         start_date, data = line.split(',', maxsplit=1)
@@ -6,5 +13,7 @@ with open("ibm-reddit-fetches.txt", "r", encoding = "utf-8") as f:
         for entry in data:
             if "subreddit" in entry.keys():
                 print(entry['created_utc'])
-                print(entry['title'])
-                print(entry['selftext'])
+                # print(entry['title'])
+                # print(entry['selftext'])
+                print(sentiment(entry['title']))
+    
