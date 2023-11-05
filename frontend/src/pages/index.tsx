e1ee90c80fe40f716584c7ab8fff03daddbe47d9
qwertysoldier <nsuttonmain@gmail.com>
2023-11-05 10:12:20 -0600

    frontend

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -31,22 +31,56 @@ export default function Home() {
     },
   });
 
-  function handleSumbit(event: any) {
-    event.preventDefault(); // Prevent the default form submission behavior
-    // Changes to real name
-    setSuccess(true);
-    // Sets real data
-    setData({
-      "1": [22, 2, 2423, 4],
-      "2": [1, 12, 3, 4],
-      "3": [21, 2, 3, 14],
-      "4": [39, 22, 23, 4],
-      "5": [11, 2, 3, 4],
+  // function handleSumbit(event: any) {
+  //   event.preventDefault(); // Prevent the default form submission behavior
+  //   // Changes to real name
+  //   setSuccess(true);
+  //   // Sets real data
+  //   setData({
+  //     "1": [22, 2, 2423, 4],
+  //     "2": [1, 12, 3, 4],
+  //     "3": [21, 2, 3, 14],
+  //     "4": [39, 22, 23, 4],
+  //     "5": [11, 2, 3, 4],
+  //   });
+  //   // Sets real news results
+  //   setNewsResult(3)
+  //   setSocialResult(1)
+  // }
+  async function handleSumbit(event: any) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  // Make a GET request to the API URL
+  await fetch('https://api.example.com/data') // Replace with your API endpoint
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Assuming the API returns JSON data
+    })
+    .then(data => {
+      // Handle the API data
+      setSuccess(true);
+
+      // Assuming the API response data has a structure similar to your example data
+      setData({
+        "1": data[0],
+        "2": data[1],
+        "3": data[2],
+        "4": data[3],
+        "5": data[4],
+      });
+
+      // You may also need to fetch and set the news and social results from the API
+      // Replace 'newsResult' and 'socialResult' with the actual API data properties
+      setNewsResult(data.newsResult);
+      setSocialResult(data.socialResult);
+    })
+    .catch(error => {
+      console.error('Error fetching data from the API:', error);
     });
-    // Sets real news results
-    setNewsResult(3)
-    setSocialResult(1)
-  }
+}
+
 
   return (
     <Container>
