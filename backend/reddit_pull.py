d811a35409dfda2521f99a4df5e4ce0668cf0131
SandyCreek <xiezhuoli771@gmail.com>
2023-11-05 00:23:08 -0500

    pending bucket placement

--- backend/reddit_pull.py
+++ backend/reddit_pull.py
@@ -1,5 +1,6 @@
 import praw
 import json
+import requests
 
 reddit = praw.Reddit(client_id="RhpaRfz1w6PUPuec825RrA", 
                      client_secret = "SIf5An9vzWPNHpDzxnvFX2W_xCfrOw",
@@ -10,7 +11,7 @@ print(subreddit.top(limit = 10))
 for submission in subreddit.top(limit = 10):
     print(submission.title)
 """
-
+"""
 with open("ibm-reddit-2.json", "r") as f:
     cnt = 0
     data = json.load(f)
@@ -18,3 +19,26 @@ with open("ibm-reddit-2.json", "r") as f:
         cnt += 1
         print(post['data']['title'])
     print(cnt)
+"""
+
+comments = [[] for _ in range(20)]
+
+def comment_full(comments):
+    for comment in comments:
+        if len(comment) < 20:
+            return False
+    return True
+
+name = "ibm"
+request = requests.get("https://www.reddit.com/search.json?q=" + name)
+# print(request.json())
+req = request.json()
+
+while not comment_full(comments) and not req['after'] == None:
+    for post in req['data']['children']:
+        submission = post['data']
+        
+        
+
+    request = requests.get("https://www.reddit.com/search.json?q=" + name + "&after=" + req['after'])
+    req = request.json()
